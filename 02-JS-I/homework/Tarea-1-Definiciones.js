5f11b2e81e713fc7248ac2e99874e8d0ad4628c9
nadamasfacil <nadamasfacil.mb@gmail.com>
2022-01-11 17:36:14 -0700

    Finish the firts homework

---
+++ 02-JS-I/homework/Tarea-1-Definiciones.js
@@ -0,0 +1,30 @@
+// Variable
+// Una Variable es un espacio determinado donde se guarda un dato ya sea un valor numérico, un nombre o algo más
+// 
+// Strings
+// Un String es una cadena o una secuencia de caracteres sin importar que tipo de caracteres sean y si
+// tienen o no un significado
+//
+// Función
+// Una función se puede describir como una o varias instrucciones que tiene que ser ejecutadas para obtener  una 
+// acción o resultado.
+// Las funciones pueden o no tener Argumentos y además pueden o no regresar un resultado
+//
+// Argumentos
+// Los Argumentos son elementos que complementan a las funciones. Estos ayudan al proceso de ejecución de las
+// instrucciones dando valores y datos que son necesarios para obtener el resultado deseado.
+//
+// Instrucción RETURN
+// La instrucción RETURN nos sirve para indicar el dato o valor que debe ser tomado como resultado para la Función
+// Por ejemplo 
+// RETURN 5
+// Esta instrucción nos indica que el resultado de la Función es 5
+//
+// Declaraciones IF
+// Una Declaración IF es un enunciado de controlador de ejecuciones. Es decir indica que ruta de ejecución de 
+// instrucciones debe seguir el proceso. Una Declaración IF solo puede obtener dos valores posibles TRUE (verdaderu)
+// o FALSE (falso)
+//
+// Valores Booleanos
+// Los valores Booleanos son aquellos que sólo pueden tener dos opciones. Son los denominados como True (verdadero) 
+// y False (falso).
