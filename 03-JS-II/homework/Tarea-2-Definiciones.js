3e29b92e5f40a8d5f81bcba3121d31fb348070cc
nadamasfacil <nadamasfacil.mb@gmail.com>
2022-01-14 20:16:52 -0700

    Finish homework 2

---
+++ 03-JS-II/homework/Tarea-2-Definiciones.js
@@ -0,0 +1,13 @@
+// for
+// La instrucción for sirve para hacer una ejecución repetitiva de un conjunto 
+// de instrucciones. Debe contener una condicional que indique hasta que punto 
+// se debe continuar con la ejecución del conjunto de instrucciones.
+//
+// &&, ||, !
+// &&, ||, ! son los llamados operadores lógicos.
+// Un operador lógico es un palabra que sirve para completar y evaluar una o 
+// varias expresiones condicionales. Estos son && (y), || (o) y ! (no es).
+// Se utilizan de la siguiente forma:
+// Condición 1  y (&&)  Condicion 2
+// Condición 1  o (||)  Condicion 2
+// No es (1)  Condicion
