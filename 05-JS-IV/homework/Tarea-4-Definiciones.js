60a6449140d7a1cb3f6e4b8ac194d8de2e464bc2
nadamasfacil <nadamasfacil.mb@gmail.com>
2022-01-21 16:02:17 -0700

    The end homework 4

---
+++ 05-JS-IV/homework/Tarea-4-Definiciones.js
@@ -0,0 +1,24 @@
+// Objetos
+// Un Objeto es una colección de datos y/o funciones relacionados
+// Son como un Arreglo pero con características más específicas llamadas
+// propiedades en lugar de subíndices.
+//
+// Propiedades
+// Se les llama Propiedades a los nombres que se asignan a una característica
+// en especial al tipo de dato o función en el Objeto.
+//
+// Métodos
+// Los Metodos son los datos tipo Funcion() en un Objeto.
+// Estas funciones siguen un conjunto de instrucciones para obtener un resuktado.
+//
+// Bucle for...in
+// El bucle for...in es un bucle similar al for.. normal que es realizar un 
+// número determinado de iteracciones nada mas que cada iterección no es contabilizada
+// por un númerp entero sino por las propiedades que componen un objeto
+//
+// Notación de puntos vs notación de corchetes
+// La Notación de puntos como su nombre lo indica es cuando hacemos referencia a
+// una propiedad de un objeto separada por un punto. Ejemplo objeto.propiedad
+// La notación de corchetes es cuando utilizamos los simbolos de abrir y cerrar 
+// corchetes para encapsular a una propiedad. Ejemplo objeto['propiedad']
+  
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = {
+    nombre : nombre,
+    edad : edad,
+    meow : function() {
+      return "Meow!"
+    }
+  }
+  return gato;
 }
 
 
@@ -14,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,13 +31,14 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  return objetoMisterioso.numeroMisterioso * 5;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -35,19 +46,28 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var nuevoobj = {
+    nombre : nombre,
+    email : email,
+    password : password
+  }
+  return nuevoobj;
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if ( usuario.email !== null && usuario.email !== undefined ) return true;
+  return false;
 }
 
 
@@ -57,6 +77,8 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( objeto[propiedad] !== undefined ) return true;
+  return false;
 }
 
 function verificarPassword(usuario, password) {
@@ -64,12 +86,16 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if ( usuario.password == password ) return true;
+  return false;
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -77,6 +103,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -85,6 +113,12 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  var n = usuarios.length;
+  if ( n > 0 )
+  for ( var i = 1; i <= n; i++ ) {
+    usuarios[i-1]['esPremium'] = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -94,11 +128,17 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  n = usuario.posts.length;
+  sumalikes = 0;
+  for ( var i = 0; i < n; i++)
+  sumalikes += usuario.posts[i].likes;
+  return sumalikes;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
-  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y 
+  // "porcentajeDeDescuento" para obtener el descuento
   // El método resta el descuento del precio y devuelve el precio con descuento
   // Devuelve el objeto "producto" al final de la función
   // Ejemplo:
@@ -106,7 +146,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+      var precioConDescuento = producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+      return precioConDescuento;
+    }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
