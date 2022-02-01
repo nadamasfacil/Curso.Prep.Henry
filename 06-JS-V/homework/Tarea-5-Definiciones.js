51e7be4e5c1c17d3213623878f97ba148fe76c19
nadamasfacil <nadamasfacil.mb@gmail.com>
2022-01-31 18:01:23 -0700

    Ending homework 5

---
+++ 06-JS-V/homework/Tarea-5-Definiciones.js
@@ -0,0 +1,8 @@
+// Prototype
+// El Prototype 'prototipo' es la forma en como esta estructurado un objeto en sus
+// propiedades. Es decir que caracteristicas tiene un objeto en particular.
+// Pueden estar anidados o sea que puede exirtir el prototipo del prototipo del prototipo, etc.
+//
+// Constructor (de Clases)
+// Definimos a un Constructor como el método por el cual asignamos las propiedades a
+// un objeto para crear su prototipo.
