03be27b7ac1ad71774cb6ecfb8e8017c568c3ae4
nadamasfacil <nadamasfacil.mb@gmail.com>
2022-02-05 19:04:37 -0700

    This is the end of homework six

---
+++ 07-JS-VI/homework/Tarea-Definiciones-6.js
@@ -0,0 +1,5 @@
+// Funciones CallBack
+// Las funciones CallBack son aquellas funciones que se utilizan como parámetros de otras
+// funciones. Para enterlo mejor decimos que una función necesita de ciertos valores para 
+// poder ejecutarse y uno de estos valores es dado por otra función, entonces la función que
+// suministra el valor faltante es esta función CallBack.
